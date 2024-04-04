6d32dca6b7a3ec9a23772e8c14870d431abcf261
Oliver Hendricks <olliehendricks04@gmail.com>
2024-04-04 10:40:41 -0400

    Completed has_adjacent_duplicates

--- Lab Problem 9.2/Source.cpp
+++ Lab Problem 9.2/Source.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 bool is_sorted_increasing(int int_array[6]);
 bool is_sorted_decreasing(int int_array[6]);
+bool has_adjacent_duplicates(int int_array[6]);
 
 int main() {
 
@@ -33,6 +34,11 @@ int main() {
 	else
 		cout << "The data are not decreasing" << endl;
 
+	if (has_adjacent_duplicates(int_array) == true)
+		cout << "The data has adjacent duplicates" << endl;
+	else
+		cout << "The data does not have adjacent duplicates" << endl;
+
 }
 
 bool is_sorted_increasing(int int_array[6]) { 
@@ -43,6 +49,12 @@ bool is_sorted_increasing(int int_array[6]) {
 
 bool is_sorted_decreasing(int int_array[6]) {
 	if (int_array[0] >= int_array[1] && int_array[1] >= int_array[2] && int_array[2] >= int_array[3] && int_array[3] >= int_array[4] && int_array[4] >= int_array[5])
+		return true;
+	else return false;
+}
+
+bool has_adjacent_duplicates(int int_array[6]) {
+	if (int_array[0] == int_array[1] || int_array[1] == int_array[2] || int_array[2] == int_array[3] || int_array[3] == int_array[4] || int_array[4] == int_array[5])
 		return true;
 	else return false;
 }
